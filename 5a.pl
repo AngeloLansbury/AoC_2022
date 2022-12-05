3026e9991a5763011602d70edd089c9bc842afd3
AngeloLansbury <119609992+AngeloLansbury@users.noreply.github.com>
2022-12-05 00:32:12 -0500

    Create 5a.pl

---
+++ 5a.pl
@@ -0,0 +1,16 @@
+@s = ([],[],[],[],[],[],[],[],[],[]);
+while (<STDIN>) {
+  last if /\d/;
+  for $c (0 .. 8) {
+    $x = substr($_, 1 + $c * 4, 1);
+    unshift(@{$s[$c + 1]}, $x) if $x ne ' ';
+  }
+}
+$_ = <STDIN>;
+while (<STDIN>) {
+  /move (\d+) from (\d+) to (\d+)/;
+  push(@{$s[$3]}, reverse(splice(@{$s[$2]}, -$1)));
+}
+for (1 .. 9) {
+  print pop(@{$s[$_]});
+}
